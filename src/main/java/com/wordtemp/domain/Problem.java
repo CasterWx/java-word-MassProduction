53f7426d41ab3f0134b45fe71f0e2b4a92194ac2
CasterWx <1325200471@qq.com>
2018-12-04 22:14:31 +0800

    add problemDaoImpl

---
+++ src/main/java/com/wordtemp/domain/Problem.java
@@ -0,0 +1,48 @@
+package com.wordtemp.domain;
+
+/**
+ * @author CasterWx  AntzUhl
+ * @site https://github.com/CasterWx
+ * @company Henu
+ * @create 2018-11-15-16:01
+ */
+public class Problem {
+    private Integer id ;
+    private String qid ;
+    private String title ;
+
+    @Override
+    public String toString() {
+        return "Problem{" +
+                "id=" + id +
+                ", qid='" + qid + '\'' +
+                ", title='" + title + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getQid() {
+        return qid;
+    }
+
+    public void setQid(String qid) {
+        this.qid = qid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+}
